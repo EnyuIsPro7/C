28ec5046d667a99b25a3608bd5911d2e4cbe6ed5
EnyuIsPro7 <iirm.rajnish@gmail.com>
2026-02-19 12:20:43 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int age = 10;
+	char name[] = "Shikhar";
+	int class = 5;
+	char section = 'F';
+
+	printf("Hello, my name is %s. I am %d years old. I study in %dth grade, section %c.", name, age, class, section);
+
+	return 0;
+}
